6abce876bad62d8a3b0ce89629f7fd057630ece6
fsuomins <fsuomins@student.42sp.org.br>
2023-10-24 20:55:08 -0300

    wip: validate update

--- srcs/validation/validate_plane.c
+++ srcs/validation/validate_plane.c
@@ -6,44 +6,56 @@
 /*   By: fsuomins <fsuomins@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 11:39:39 by fsuomins          #+#    #+#             */
-/*   Updated: 2023/10/19 12:02:08 by fsuomins         ###   ########.fr       */
+/*   Updated: 2023/10/24 20:50:00 by fsuomins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minirt.h"
 
+static void	validate_plane_orientation(char *line)
+{
+	char	**split;
+
+	split = ft_split(line, ',');
+	if (!split[0] || (ft_atof(split[0]) < -1.0 && ft_atof(split[0]) > 1.0))
+		exit_error("Invalid plane orientation.\n");
+	if (!split[1] || (ft_atof(split[1]) < -1.0 && ft_atof(split[1]) > 1.0))
+		exit_error("Invalid plane orientation.\n");
+	if (!split[2] || (ft_atof(split[2]) < -1.0 && ft_atof(split[2]) > 1.0))
+		exit_error("Invalid plane orientation.\n");
+}
+
+static void	validate_plane_position(char *line)
+{
+	char	**split;
+
+	split = ft_split(line, ',');
+	if (!split[0])
+		exit_error("Invalid plane position.\n");
+	if (!split[1])
+		exit_error("Invalid plane position.\n");
+	if (!split[2])
+		exit_error("Invalid plane position.\n");
+}
+
 void	validate_plane(char *line)
 {
-	char	*str;
-	int		count;
-	int		value;
-	int		value;
-	int		value;
+	char	**split;
 
-	count = 0;
-	while ((str = ft_strtok(line, " ")) != NULL)
-	{
-		if (count == 0 || count == 1 || count == 2)
-		{
-			value = ft_atoi(str);
-			if (value < -180 || value > 180)
-				exit_error("Invalid plane position.\n");
-		}
-		else if (count == 3)
-		{
-			value = ft_atoi(str);
-			if (value < -180 || value > 180)
-				exit_error("Invalid plane orientation.\n");
-		}
-		else if (count == 4)
-		{
-			value = ft_atoi(str);
-			if (value < 0 || value > 255)
-				exit_error("Invalid plane color.\n");
-		}
-		str = NULL;
-		count++;
-	}
-	if (count != 5)
-		exit_error("Invalid plane definition.\n");
+	line++;
+	printf("%s\n", line);
+	while (*line == ' ')
+		line++;
+	split = ft_split(line, ' ');
+	if (*line == '\0' || *line == '\n')
+		exit_error("Invalid plane.\n");
+	if (!split[1])
+		exit_error("Invalid plane.\n");
+	if (!split[2])
+		exit_error("Invalid plane.\n");
+	if (!split[3])
+		exit_error("Invalid plane.\n");
+	validate_plane_position(split[0]);
+	validate_plane_orientation(split[1]);
+	validate_color(split[2]);
 }
--- srcs/validation/validate_sphere.c
+++ srcs/validation/validate_sphere.c
@@ -6,42 +6,51 @@
 /*   By: fsuomins <fsuomins@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 11:15:52 by fsuomins          #+#    #+#             */
-/*   Updated: 2023/10/19 20:25:50 by fsuomins         ###   ########.fr       */
+/*   Updated: 2023/10/24 20:49:55 by fsuomins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minirt.h"
 
+static void	validate_sphere_radius(char *line)
+{
+	char	**split;
+
+	split = ft_split(line, ' ');
+	if ((ft_atof(split[0]) < 0.0))
+		exit_error("Invalid sphere radius.\n");
+}
+
+static void	validate_sphere_position(char *line)
+{
+	char	**split;
+
+	split = ft_split(line, ',');
+	if (!split[0])
+		exit_error("Invalid sphere position.\n");
+	if (!split[1])
+		exit_error("Invalid sphere position.\n");
+	if (!split[2])
+		exit_error("Invalid sphere position.\n");
+}
+
 void	validate_sphere(char *line)
 {
-	char	*str;
-	int		count;
-	int		value;
+	char	**split;
 
-	count = 0;
-	while ((str = strtok(line, " ")) != NULL)
-	{
-		if (count == 0 || count == 1 || count == 2)
-		{
-			value = atoi(str);
-			if (value < -180 || value > 180)
-				exit_error("Invalid sphere position.\n");
-		}
-		else if (count == 3)
-		{
-			value = atoi(str);
-			if (value < 0)
-				exit_error("Invalid sphere radius.\n");
-		}
-		else if (count == 4)
-		{
-			value = atoi(str);
-			if (value < 0 || value > 255)
-				exit_error("Invalid sphere color.\n");
-		}
-		str = NULL;
-		count++;
-	}
-	if (count != 5)
-		exit_error("Invalid sphere definition.\n");
+	line++;
+	while (*line == ' ')
+		line++;
+	split = ft_split(line, ' ');
+	if (*line == '\0' || *line == '\n')
+		exit_error("Invalid sphere.\n");
+	if (!split[1])
+		exit_error("Invalid sphere.\n");
+	if (!split[2])
+		exit_error("Invalid sphere.\n");
+	if (!split[3])
+		exit_error("Invalid sphere.\n");
+	validate_sphere_position(split[0]);
+	validate_sphere_radius(split[1]);
+	validate_color(split[2]);
 }
